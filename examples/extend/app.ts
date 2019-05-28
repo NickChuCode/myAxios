c451b86349cc7f68b9772cffdfc9ad4220cd5fb0
NickChuCode <nickchu15@outlook.com>
2019-05-28 14:39:09 +0800

    feat(src, examples): 实现 axios 函数重载，响应数据支持泛型

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -1,10 +1,34 @@
 import myAxios from '../../src/index'
 
+interface ResponseData<T = any> {
+    code: number
+    result: T
+    message: string
+}
+
+interface User {
+    name: string
+    age: number
+}
+
+function getUser<T>() {
+    return myAxios<ResponseData<T>>('/extend/user')
+        .then(res => res.data)
+        .catch(err => console.error(err))
+}
+
 myAxios({
     url: '/extend/post',
     method: 'post',
     data: {
         msg: 'hi, myAxios post'
+    }
+})
+
+myAxios('/extend/post',{
+    method: 'post',
+    data: {
+        msg: 'hi, myAxios post with 2 arguments'
     }
 })
 
@@ -29,3 +53,12 @@ myAxios.post('/extend/post', { msg: 'post' })
 myAxios.put('/extend/put', { msg: 'put' })
 
 myAxios.patch('/extend/patch', { msg: 'patch' })
+
+async function test() {
+    const user = await getUser<User>()
+    if (user) {
+        console.log(user.result.name)
+    }
+}
+
+test()
