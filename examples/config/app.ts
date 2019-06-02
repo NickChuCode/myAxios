fe8e75cfeafc9f5ae99b5e8ecd30213ebc9a80f2
NickChuCode <nickchu15@outlook.com>
2019-06-02 23:23:56 +0800

    feat(src/axios.ts, examples/config): 为 axios 提供 create 静态接口，可以创建一个新的 axios 实例

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -16,7 +16,29 @@ import qs from 'qs'
 // }).then(res => {
 //     console.log(res.data)
 // })
-myAxios({
+// myAxios({
+//     transformRequest: [(function(data) {
+//         return qs.stringify(data)
+//     }), ...(myAxios.defaults.transformRequest as AxiosTransformer[])],
+//     transformResponse: [...(myAxios.defaults.transformResponse as AxiosTransformer[]), function(data) {
+//         if (typeof data === 'object') {
+//             data.b = 2
+//         }
+//         return data
+//     }],
+//     // headers: {}, // 目前代码有点问题，必须要传入一个空的 headers，目前没有找到问题所在
+//     // 已经解决，mergeConfig 中的 deepMergeStrat 写的不完善，没有考虑 val2 为 undefined 的合并
+//     // 顺便，debugger 真的很好用
+//     url: '/config/post',
+//     method: 'post',
+//     data: {
+//         a: 1
+//     }
+// }).then((res) => {
+//     console.log(res.data)
+// })
+
+const instance = myAxios.create({
     transformRequest: [(function(data) {
         return qs.stringify(data)
     }), ...(myAxios.defaults.transformRequest as AxiosTransformer[])],
@@ -25,10 +47,10 @@ myAxios({
             data.b = 2
         }
         return data
-    }],
-    // headers: {}, // 目前代码有点问题，必须要传入一个空的 headers，目前没有找到问题所在
-    // 已经解决，mergeConfig 中的 deepMergeStrat 写的不完善，没有考虑 val2 为 undefined 的合并
-    // 顺便，debugger 真的很好用
+    }]
+})
+
+instance({
     url: '/config/post',
     method: 'post',
     data: {
--- src/axios.ts
+++ src/axios.ts
@@ -1,9 +1,10 @@
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosStatic } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/util'
 import defaults from './defaults'
+import mergeConfig from './core/mergeConfig'
 
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
   // 首先，instance 就是一个函数，就是 request 绑定对象的 this的函数，可以调用
   const instance = Axios.prototype.request.bind(context)
@@ -11,9 +12,13 @@ function createInstance(config: AxiosRequestConfig): AxiosInstance {
   // 其次，通过 extend，将 context 对象上的所有属性和方法都给 instance，这样，instance 就又是一个对象
   extend(instance, context)
 
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const myAxios = createInstance(defaults)
+
+myAxios.create = function create(config) {
+  return createInstance(mergeConfig(defaults, config))
+}
 
 export default myAxios
