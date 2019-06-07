8db6ef1c65fc5f69913df08396d7af9cf587a41a
NickChuCode <nickchu15@outlook.com>
2019-06-07 23:15:48 +0800

    添加上传下载相关注释

--- src/xhr.ts
+++ src/xhr.ts
@@ -3,6 +3,7 @@ import { parseHeaders } from './helpers/headers'
 import { createError } from './helpers/error'
 import { isURLSameOrigin } from './helpers/url'
 import cookie from './helpers/cookie'
+import {isFormData} from "./helpers/util";
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
@@ -16,63 +17,136 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       cancelToken,
       withCredentials,
       xsrfCookieName,
-      xsrfHeaderName
+      xsrfHeaderName,
+      onDownloadProgress,
+      onUploadProgress
     } = config
 
+    /*
+    * 发送请求的整个流程分为下面7步
+    * 创建一个 request 实例。
+    * 执行 request.open 方法初始化。
+    * 执行 configureRequest 配置 request 对象。
+    * 执行 addEvents 给 request 添加事件处理函数。
+    * 执行 processHeaders 处理请求 headers。
+    * 执行 processCancel 处理请求取消逻辑。
+    * 执行 request.send 方法发送请求。
+    * */
     const request = new XMLHttpRequest()
-
-    if (responseType) {
-      request.responseType = responseType
-    }
-
-    if (timeout) {
-      request.timeout = timeout
-    }
-
-    if (withCredentials) {
-      request.withCredentials = withCredentials
-    }
-
-    if (cancelToken) {
-      cancelToken.promise.then(reason => {
-        request.abort()
-        reject(reason)
-      })
-    }
-
-    if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName) {
-      const xsrfValue = cookie.read(xsrfCookieName)
-      if (xsrfValue) {
-        headers[xsrfHeaderName!] = xsrfValue
-      }
-    }
 
     request.open(method.toUpperCase(), url!, true)
 
-    request.onreadystatechange = function handleLoad() {
-      if (request.readyState !== 4) {
-        return
+    configureRequest()
+
+    addEvents()
+
+    processHeaders()
+
+    processCancel()
+
+    request.send(data)
+
+
+
+
+
+    // 下面是具体的实现代码
+    function configureRequest(): void {
+      if (responseType) {
+        request.responseType = responseType
       }
 
-      // 当出现网络错误或者超时错误的时候，该值都为 0
-      if (request.status === 0) {
-        return
+      if (timeout) {
+        request.timeout = timeout
       }
 
-      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
-      const responseData =
-        responseType && responseType !== 'text'
-          ? request.response
-          : request.responseText
-      const response: AxiosResponse = {
-        data: responseData,
-        status: request.status,
-        statusText: request.statusText,
-        headers: responseHeaders,
-        config,
-        request
+      if (withCredentials) {
+        request.withCredentials = withCredentials
       }
-      handleResponse(response)
+    }
+
+    function addEvents(): void {
+      request.onreadystatechange = function handleLoad() {
+        if (request.readyState !== 4) {
+          return
+        }
+
+        // 当出现网络错误或者超时错误的时候，该值都为 0
+        if (request.status === 0) {
+          return
+        }
+
+        const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+        const responseData =
+            responseType && responseType !== 'text'
+                ? request.response
+                : request.responseText
+        const response: AxiosResponse = {
+          data: responseData,
+          status: request.status,
+          statusText: request.statusText,
+          headers: responseHeaders,
+          config,
+          request
+        }
+        handleResponse(response)
+      }
+
+      request.onerror = function handleError() {
+        reject(createError('Network Error', config, null, request))
+      }
+
+      request.ontimeout = function handleTimeout() {
+        reject(
+            createError(
+                `Timeout of ${timeout}ms exceeded`,
+                config,
+                'ECONNABORTED',
+                request
+            )
+        )
+      }
+
+      if (onDownloadProgress) {
+        request.onprogress = onDownloadProgress
+      }
+
+      if (onUploadProgress) {
+        request.upload.onprogress = onUploadProgress
+      }
+    }
+
+    function processHeaders(): void {
+      if (isFormData(data)) {
+        delete headers['Content-Type']
+      }
+
+      if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName) {
+        const xsrfValue = cookie.read(xsrfCookieName)
+        if (xsrfValue) {
+          headers[xsrfHeaderName!] = xsrfValue
+        }
+      }
+
+      Object.keys(headers).forEach(name => {
+        if (data === null && name.toLowerCase() === 'content-type') {
+          // 这里要额外判断一个逻辑，当我们传入的 data 为空的时候，
+          // 请求 header 配置 Content-Type 是没有意义的，于是我们把它删除
+          delete headers[name]
+        } else {
+          // 将处理后的 headers 放入请求
+          request.setRequestHeader(name, headers[name])
+        }
+      })
+    }
+
+    function processCancel(): void {
+      if (cancelToken) {
+        cancelToken.promise.then(reason => {
+          request.abort()
+          reject(reason)
+        })
+      }
     }
 
     function handleResponse(response: AxiosResponse) {
@@ -91,32 +165,5 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       }
     }
 
-    request.onerror = function handleError() {
-      reject(createError('Network Error', config, null, request))
-    }
-
-    request.ontimeout = function handleTimeout() {
-      reject(
-        createError(
-          `Timeout of ${timeout}ms exceeded`,
-          config,
-          'ECONNABORTED',
-          request
-        )
-      )
-    }
-
-    Object.keys(headers).forEach(name => {
-      if (data === null && name.toLowerCase() === 'content-type') {
-        // 这里要额外判断一个逻辑，当我们传入的 data 为空的时候，
-        // 请求 header 配置 Content-Type 是没有意义的，于是我们把它删除
-        delete headers[name]
-      } else {
-        // 将处理后的 headers 放入请求
-        request.setRequestHeader(name, headers[name])
-      }
-    })
-
-    request.send(data)
   })
 }
